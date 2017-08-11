f03730f537395878c6d4b117173c63134ea184a2
Kacper <kacperkozbial@onet.pl>
2017-08-11 08:03:08 +0200

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,12 @@
+package com.esej.donotcopy;
+string info = "bez importow"
+string import1 = "hello";
+public class blabla
+	public void onChat(AsyncChat...); {
+		sender.sendMessage ("Nie polecam kopiowac kodu i wklejac od razu do eclipsa etc.. bo to skutkuje zepsuciem pluginu :D")
+		}
+	public void onChat(ASyncChat...); {
+		sender.sendMessage (String import1)
+	}
+
+# NIE UMIEM PISAC PLUGINUFF! NUDAAA o 08;02 coooo robi ć????????
